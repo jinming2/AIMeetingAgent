b4e087583f1f4ad6c8a2db4739838aa06f25bd1f
Qi <145955414+qijing223@users.noreply.github.com>
2025-04-15 21:12:58 -0500

    start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 echo "1. Generate .env"
 cat > .env <<EOF
-AZURE_SPEECH_KEY=your_azure_key_here
-AZURE_SPEECH_REGION=your_azure_region_here
+AZURE_SPEECH_KEY=QydWb4z8xr4cjHR7MRikoBGV1fvZJ75vJjeGlh3QPXHf9yUaeryiJQQJ99BDAC4f1cMXJ3w3AAAYACOGaqxp
+AZURE_SPEECH_REGION=westus
 EOF
 echo ".env generated"
 
